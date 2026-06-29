359fe9831800a8b15f05eab33d4348862bb28932
吴秋松 <806819774@qq.com>
2026-06-29 15:47:20 +0800

    新增：用户端本地接口代理

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,21 @@
 module.exports = {
 	productionSourceMap: false, // 生产打包时不输出map文件，增加打包速度
+	devServer: {
+		proxy: {
+			'/api': {
+				target: 'http://127.0.0.1:8018',
+				changeOrigin: true,
+			},
+			'/uploads': {
+				target: 'http://127.0.0.1:8018',
+				changeOrigin: true,
+			},
+			'/statics': {
+				target: 'http://127.0.0.1:8018',
+				changeOrigin: true,
+			},
+		},
+	},
 	configureWebpack: config => {
 		if (process.env.NODE_ENV === 'production') {
 			config.optimization.minimizer[0].options.terserOptions.compress.warnings = false
